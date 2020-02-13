531e8ad550f4653f0f43717d1b60ab0999960abd
Rytautas <rytautas.kvasinskas@gmail.com>
2020-02-13 17:20:14 +0200

    chaged for e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-demo!');
+    expect(page.getTitleText()).toEqual('no no no message');
   });
 
   afterEach(async () => {
